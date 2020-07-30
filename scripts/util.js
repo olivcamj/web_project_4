e976d085fe6eaf0dc30e612e61dd8c9c86fe05a4
Olivia Cameron <oliviacameron@Olivias-Air.lan1>
2020-07-30 15:38:26 -0400

    add: create util.js file

---
+++ scripts/util.js
@@ -0,0 +1,4 @@
+export const cardPopupImg = document.querySelector('.modal__img');
+        
+//const cardTitle = cardElement.querySelector('.card__text');
+export const cardTitle = document.querySelector('.modal__caption');
