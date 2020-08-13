61a273f0ba9ccf68d9b434b98ee2d47e2877b3a1
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-08-13 15:36:32 -0400

    refactor toggleModalWindow()

---
+++ scripts/Popup.js
@@ -0,0 +1,31 @@
+class Popup {
+    constructor(popupSelector) {
+        this._popupElement = popupSelector;
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+
+    open(){
+        this._popupElement.classList.add('modal_visible');
+        document.addEventListener('keyup', this._handleEscClose);
+    }
+
+    close(){
+        this._popupElement.classList.remove('modal_visible');
+        document.removeEventListener('keyup', this._handleEscClose)
+    }
+
+    _handleEscClose() {
+        if (e.which == 27){
+            this.close();
+        }
+
+    }
+
+    setEventListeners() {
+        this._popupElement.querySelector('.modal_visble').addEventListener('click', (e) => {
+            this.close();
+        });
+    }
+}
+
+export default Popup;
