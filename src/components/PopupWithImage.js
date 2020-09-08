f5aad43693e2e8ed6a177ad48a5c57571362758e
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-09-07 22:54:31 -0400

    fix: doesn't need an explicit constructor

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,9 +1,6 @@
 import Popup from './Popup.js';
 
 class PopupWithImage extends Popup {
-    constructor(popupSelector) {
-        super(popupSelector);
-    }
 
     open({ link, name }) {
         this._popupElement.querySelector('.modal__img').src = link;
