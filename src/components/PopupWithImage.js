995394b3e78ea0e58913d2e313f7fb6612804aca
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-09-07 22:59:00 -0400

    feat: update the img alt attribute

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,6 +4,7 @@ class PopupWithImage extends Popup {
 
     open({ link, name }) {
         this._popupElement.querySelector('.modal__img').src = link;
+        this._popupElement.querySelector('.modal__img').alt = name;
         this._popupElement.querySelector('.modal__caption').textContent = name;
 
         super.open();
