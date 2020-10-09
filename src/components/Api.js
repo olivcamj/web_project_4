f4d33be55a80248c58c9b67fa85ca282c0038ff7
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-10-09 17:06:50 -0400

    feat(like): fetch Like status and reintroduce ()'s

--- src/components/Api.js
+++ src/components/Api.js
@@ -5,19 +5,19 @@ class Api {
     }
 
     getInitialCards() {
-        return fetch( this._baseUrl + '/cards', {
+        return fetch(this._baseUrl + '/cards', {
             headers: this._headers
         })
-        .then(res => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
-        .catch(err => console.log(err))
+        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .catch((err) => console.log(err))
     }
 
     getUserInfo() {
-        return fetch( this._baseUrl + '/users/me', {
+        return fetch(this._baseUrl + '/users/me', {
             headers: this._headers
         })
-        .then(res => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
-        .catch(err => console.log(err))
+        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .catch((err) => console.log(err))
     }
 
     addCard({ name, link }) {
@@ -29,8 +29,8 @@ class Api {
                 link
             })
         })
-        .then(res => res.ok ? res.json() : Promise.reject('Error' + res.statusText))
-        .catch(err => console.log(err))
+        .then((res) => {res.ok ? res.json() : Promise.reject('Error' + res.statusText)})
+        .catch((err) => console.log(err))
     }
     
     deleteCard(cardId) {
@@ -38,8 +38,8 @@ class Api {
             headers: this._headers,
             method: "DELETE"
         })
-        .then(res => {res.ok ? res.json() : Promise.reject('Error!' + res.statusText)})
-        .catch(res => console.log(res))
+        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .catch((err) => console.log(err))
     }
 
     editUserInfo(data) {
@@ -51,9 +51,25 @@ class Api {
                 about: data.title
             })
         })
-        .then(res => {res.ok ? res.json() : Promise.reject('Error!' + res.statusText)})
-        .catch(res => console.log(res))
+        .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+        .catch((err) => console.log(err))
+    }
+
+    changeLikeStatus(cardId, boolean) {
+        if (boolean) {
+            return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+            headers: this._headers,
+            method: "PUT"}) 
+            .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+            .catch(err => console.log(err))
+
+        } else {
+            return fetch(this._baseUrl + '/cards/likes/' + cardId, {
+            headers: this._headers,
+            method: "DELETE"})
+            .then((res) => res.ok ? res.json() : Promise.reject('Error!' + res.statusText))
+            .catch(err => console.log(err))
+        }
     }
 }
-
 export default Api;
