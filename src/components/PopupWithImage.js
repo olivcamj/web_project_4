b8e690a15bb28798175645a469d8ab1deccb0bac
Olivia Cameron <oliviacameron@Olivias-MacBook-Air.local>
2020-10-24 16:54:17 -0400

    fix: create global variables for DOM elements

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,11 +1,16 @@
 import Popup from './Popup.js';
 
 class PopupWithImage extends Popup {
+    constructor(popupSelector){
+        super(popupSelector);
+        this._image = this._popupElement.querySelector('.modal__img');
+        this._caption = this._popupElement.querySelector('.modal__caption');
+    }
 
     open({ link, name }) {
-        this._popupElement.querySelector('.modal__img').src = link;
-        this._popupElement.querySelector('.modal__img').alt = name;
-        this._popupElement.querySelector('.modal__caption').textContent = name;
+        this._image.src = link;
+        this._image.alt = name;
+        this._caption.textContent = name;
 
         super.open();
     }
